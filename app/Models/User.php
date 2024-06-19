3cf5bbdf66257eb9e3ad7d8d16f81bb17621422f
Ndemafia Wilsmith <wilsmithndemafia@gmail.com>
2024-06-19 01:08:37 +0100

    Fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
-        'allergies' => 'array',
+    ];
+
+    protected $attributes = [
+        'allergies' => '[]',
     ];
 }
